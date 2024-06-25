5033cad70b2e11729c276f6171d5aec18bd8391a
unknown <22100514@ibsu.edu.ge>
2024-06-26 00:05:08 +0400

    Initial  commit

--- db.js
+++ db.js
@@ -2,14 +2,19 @@ import { connect, connection as _connection } from 'mongoose';
 
 const MONGODB_URI = 'mongodb+srv://gambusha2000:<Sfxedgs6lfs5re28>@url-shortener.mtunq3q.mongodb.net/?retryWrites=true&w=majority&appName=url-shortener';
 
-connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+async function connectDB() {
+  try {
+    await connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB database connection established successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); 
+  }
+}
 
 const connection = _connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
 
-export default connection;
+export { connectDB, connection };
